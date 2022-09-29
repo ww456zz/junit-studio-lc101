270a2ddeca6cc9e1aba7a7c0f3be2f7526ef0144
ww456zz <ww456zzdd@gmail.com>
2022-09-28 19:31:45 -0500

    first

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,9 +9,62 @@ public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
 
+    @Test
+    public void bracketWithInnerTextReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[hgfyiolg]"));
+    }
+
+    @Test
+    public void bracketsInTextWithTextInsideReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("ksahlk[mvnb,]"));
+    }
+
+    @Test
+    public void bracketsBeforeTextReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]cmhdcg"));
+    }
+
+    @Test
+    public void twoBracketsWithTextReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("jgfk[vjvhkvjhj]vbnvm[vnbm]"));
+    }
+
+    @Test
+    public void unorderedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void unclosedBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void unorderedBracketsWithTextReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("gfvjkhg]gjhjh["));
+    }
+
+    @Test
+    public void unclosedBracketWithTextReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("gkj[fghkgh"));
+    }
+
+    @Test
+    public void noOpeningBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("hk]vj"));
+    }
+
+    @Test
+    public void unclosedBracketsWithTextReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("I[love[Ruta]bagas]and[ban]ananananan[anas["));
+    }
 }
